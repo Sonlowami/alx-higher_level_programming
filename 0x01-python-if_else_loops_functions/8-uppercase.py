aa780f89908629efb22f69f9acb451bfd45888fe
Sonlowami <uwilowami64@outlook.com>
2022-09-21 17:08:59 +0200

    Fix typo in 12, use format() in 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for ch in str:
         if ord(ch) > 96 and ord(ch) < 123:
-            print(f"{chr(ord(ch) - 32)}", end='')
+            print("{:c}".format(chr(ord(ch) - 32)), end='')
         else:
             print(f"{ch}", end='')
     print("\n")
