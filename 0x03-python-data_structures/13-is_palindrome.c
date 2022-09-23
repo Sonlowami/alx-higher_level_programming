9eff5737925e9b5c88f324759b3eba75df230745
Sonlowami <uwilowami64@outlook.com>
2022-09-23 21:41:02 +0200

    Check if a list is a palindrome - the long way

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * is_palindrome - check if a linkedlist is a palindrome
+ * @head: pointer to the reference to the first node of the list
+ *
+ * Return: 1 if palindrome, 0 otherwise.
+ */
+int is_palindrome(listint_t **head)
+{
+	int len, i, x[1024];
+	listint_t *node;
+
+	if (!*head || !(*head + 1))
+		return (1);
+	node = *head, len = i = 0;
+	while (node)
+	{
+		x[len++] = node->n;
+		node = node->next;
+	}
+	len--;
+	for (; i < len; i++, len--)
+	{
+		if (x[i] != x[len])
+			return (0);
+	}
+	return (1);
+}
