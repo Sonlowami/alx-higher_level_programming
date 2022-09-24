368816bde91c91d7b81d6c3a8ee7915431aed6ae
Sonlowami <uwilowami64@outlook.com>
2022-09-24 13:52:21 +0200

    Print hexadecimals from 0 to 98

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,37 +1,3 @@
 #!/usr/bin/python3
-for number in range(99):
-    if (number % 16) < 10:
-        if not ((number // 16) == 0):
-            print(f"{number} = 0x{number // 16}{number % 16}")
-        else:
-            print(f"{number} = 0x{number % 16}")
-    elif (number % 16) == 10:
-        if not ((number // 16) == 0):
-            print(f"{number} = 0x{number // 16}a")
-        else:
-            print(f"{number} = 0xa")
-    elif (number % 16) == 11:
-        if not ((number // 16) == 0):
-            print(f"{number} = 0x{number // 16}b")
-        else:
-            print(f"{number} = 0xb")
-    elif (number % 16) == 12:
-        if not ((number // 16) == 0):
-            print(f"{number} = 0x{number // 16}c")
-        else:
-            print(f"{number} = 0xc")
-    elif (number % 16) == 13:
-        if not ((number // 16) == 0):
-            print(f"{number} = 0x{number // 16}d")
-        else:
-            print(f"{number} = 0xd")
-    elif (number % 16) == 14:
-        if not ((number // 16) == 0):
-            print(f"{number} = 0x{number // 16}e")
-        else:
-            print(f"{number} = 0xe")
-    elif (number % 16) == 15:
-        if not ((number // 16) == 0):
-            print(f"{number} = 0x{number // 16}f")
-        else:
-            print(f"{number} = 0xf")
+for n in range(99):
+    print("0x{:x}".format(n))
