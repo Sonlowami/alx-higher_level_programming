5b2faeecfbf35bc7a2213f6dd8cfa1ebaac6d665
Sonlowami <uwilowami64@outlook.com>
2022-09-24 12:14:37 +0200

    Add an interpreter at the top of py files

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     list_a, list_b = list(tuple_a), list(tuple_b)
     list_a.append(0)
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def max_integer(my_list=[]):
     maximum = my_list[0]
     for i in my_list:
