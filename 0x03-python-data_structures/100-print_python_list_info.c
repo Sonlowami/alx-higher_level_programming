fd5ffbd421183c9d34016225588593a876ed95f8
Sonlowami <uwilowami64@outlook.com>
2022-09-25 11:15:55 +0200

    Fix type bugs that interfered withcompilation

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -8,12 +8,9 @@
 void print_python_list_info(PyObject *p)
 {
 	int i;
-
-	if PyList_Check(p)
-	{
-		printf("[*] Size of the Python List = %d\n", PyList_Size(p));
-		printf("[*] Allocated = %d\n", p->allocated);
-		for (i = 0; i < PyList_Size(p); i++)
-			printf("Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i)));
-	}
+	PyListObject *px = (PyListObject *)p;
+	printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
+	printf("[*] Allocated = %ld\n", px->allocated);
+	for (i = 0; i < PyList_Size(p); i++)
+		printf("Element %d: %s\n", i, Py_TYPE(px->ob_item[i])->tp_name);
 }
