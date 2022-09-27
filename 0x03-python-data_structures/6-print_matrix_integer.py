429f9735be37c8e7f17dfd7b7b6523c6dadd6722
Sonlowami <uwilowami64@outlook.com>
2022-09-27 16:53:43 +0200

    Use an integer formatting

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for elem in row:
             if row.index(elem) < len(row) - 1:
-                print("{}".format(elem), end=' ')
+                print("{:d}".format(elem), end=' ')
             else:
-                print("{}".format(elem), end='')
+                print("{:d}".format(elem), end='')
         print("")
