19abac49dfdffdf5891b7f8184e71c1c034b5bd6
Sonlowami <uwilowami64@outlook.com>
2022-09-27 21:11:49 +0200

    Fix a bug that made only first encounter removed

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    lmy_string = list(my_string)
-    for ch in lmy_string:
-        if ch == 'c' or ch == 'C':
-            lmy_string.remove(ch)
-    my_string = ''.join(str(item) for item in lmy_string)
-    return my_string
+    new_string = ''
+    for ch in my_string:
+        if ch != 'c' and ch != 'C':
+            new_string += ch
+    return new_string
