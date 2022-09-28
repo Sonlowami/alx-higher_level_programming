01b5359a10b08ffff60ce70477368895a5d122d5
Sonlowami <uwilowami64@outlook.com>
2022-09-28 21:18:39 +0200

    Correct a typho in interpreter call and fix flake8 error

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary != None:
+    if a_dictionary is not None:
         best = list(a_dictionary.items())[0][0]
         for k, v in a_dictionary.items():
             if v > a_dictionary[best]:
                 best = k
         return best
-
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     dc = {}
     for k, v in a_dictionary.items():
