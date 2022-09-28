334b56c20eaddd2c176172e0c263d610f7b06ef8
Sonlowami <uwilowami64@outlook.com>
2022-09-28 21:22:36 +0200

    Fix case of an empty dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
+    if a_dictionary is not None and len(a_dictionary) > 0:
         best = list(a_dictionary.items())[0][0]
         for k, v in a_dictionary.items():
             if v > a_dictionary[best]:
