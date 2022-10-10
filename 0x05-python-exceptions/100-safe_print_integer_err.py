cee7cff56180770b730d23ad1c62fca04447dd2d
Sonlowami <uwilowami64@outlook.com>
2022-10-10 22:39:17 +0200

    Print integer

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import stderr
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except ValueError as v:
+        print(v.args, file=stderr)
+        return False
+    return True
