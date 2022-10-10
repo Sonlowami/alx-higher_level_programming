99ff39a35eb371fc674e5ec708720935b0071bea
Sonlowami <uwilowami64@outlook.com>
2022-10-10 22:46:10 +0200

    Remove the integer conversion

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     i = 0
     while i < x:
         try:
-            print("{:d}".format(int(my_list[i])), end="")
+            print("{:d}".format(my_list[i]), end="")
         except TypeError:
             pass
         except ValueError:
