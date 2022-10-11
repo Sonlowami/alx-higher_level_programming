1481bc6533e73b271e203687f8fd3aaaceab1496
Sonlowami <uwilowami64@outlook.com>
2022-10-11 22:35:25 +0200

    Solve problems from 1 to 5

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    """Define a square"""
+    def __init__(self, size):
+        
+        """Initialize the size of the square
+        Args:
+            size: the size of the square"""
+        self.__size = size
