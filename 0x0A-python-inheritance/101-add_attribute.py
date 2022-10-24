05022096a61950035e63d65a438678f5dbb24f30
Sonlowami <uwilowami64@outlook.com>
2022-10-25 00:05:00 +0200

    Fix a typo in error message

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,5 +5,5 @@
 def add_attribute(obj, key, value):
     """Attempt to add an attribute to an object"""
     if not hasattr(obj, '__dict__'):
-        raise TypeError("can't add a new attribute")
+        raise TypeError("can't add new attribute")
     vars(obj)[key] = value
