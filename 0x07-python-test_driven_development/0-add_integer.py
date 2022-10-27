2ea09c114108957d0bbc4248268d13893d259899
Sonlowami <uwilowami64@outlook.com>
2022-10-27 11:29:09 +0200

    Rename function

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,8 +2,9 @@
 """Adding two numbers"""
 
 
-def add(a, b=98):
+def add_integer(a, b=98):
     """Add argument A and optional Argument b"""
+
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError('a must be an integer')
     if not isinstance(b, int) and not isinstance(b, float):
