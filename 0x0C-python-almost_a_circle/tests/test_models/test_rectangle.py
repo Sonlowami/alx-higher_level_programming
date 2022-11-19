6f1650fe21666a5a0e79a7bff888d110dfd50248
Sonlowami <uwilowami64@outlook.com>
2022-11-19 22:53:39 +0200

    Add more tests for setters

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -19,48 +19,79 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(rect.x, 0)
         self.assertEqual(rect.y, 1)
 
-    def test_setters_with_positive_numbers(self):
+    def test_setters_with_2_numbers(self):
         """Test setter functions with positive and negative numbers"""
         rect = Rectangle(45, 5)
 
         self.assertEqual(rect.width, 45)
         self.assertEqual(rect.height, 5)
+
+    def test_setters_with_3_numbers(self):
+        """Test setters with three numbers"""
+        rect = Rectangle(45, 5, 1)
+
+        self.assertEqual(rect.width, 45)
+        self.assertEqual(rect.height, 5)
+        self.assertEqual(rect.x, 1)
+        self.assertEqual(rect.y, 0)
+
+    def test_setters_with_4_numbers(self):
+        """Test with width, height, x and y"""
+        rect = Rectangle(45, 5, 1, 8)
+
+        self.assertEqual(rect.width, 45)
+        self.assertEqual(rect.height, 5)
+        self.assertEqual(rect.x, 1)
+        self.assertEqual(rect.y, 8)
     
-    def test_setters_with_negative_numbers(self):
-        """With negative numbers"""
-
+    def test_width_setter1(self):
+        """Test width setter with negative value"""
         with self.assertRaises(ValueError):
-            self.rect.width = -455
-        with self.assertRaises(ValueError):
-            self.rect.height = -5
-        with self.assertRaises(ValueError):
-            self.rect.x = -2
-        with self.assertRaises(ValueError):
-            self.rect.y = -3
+            rect = Rectangle(-67, 2, 44, 4)
 
-    def test_setters_with_floats(self):
-        """Test setter functions with strings"""
+    def test_height_setter1(self):
+        """Test height setter with negative value"""
+        with self.assertRaises(ValueError):
+            rect = Rectangle(67, -2, 44, 4)
 
-        self.assertRaises(TypeError, self.rect.width, 3.145)
-        self.assertRaises(TypeError, self.rect.height, 4.21)
-        self.assertRaises(TypeError, self.rect.x, 12)
-        self.assertRaises(TypeError, self.rect.y, 0.5)
+    def test_x_setter1(self):
+        """Test x setter with negative value"""
+        with self.assertRaises(ValueError):
+            rect = Rectangle(67, 2, -44, 4)
 
-    def test_setters_with_zero_a(self):
+    def test_y_setter1(self):
+        """Test y setter with negative value"""
+        with self.assertRaises(ValueError):
+            rect = Rectangle(67, 2, 44, -4)
+
+    def test_width_float(self):
+        """Test setter functions with a float"""
+        with self.assertRaises(TypeError):
+            rect = Rectangle(3.14, 4)
+
+    def test_height_float(self):
+        """Test height setter functions with a float"""
+        with self.assertRaises(TypeError):
+            rect = Rectangle(4, 4.2)
+
+    def test_x_float(self):
+        """Test x setter functions with strings"""
+        with self.assertRaises(TypeError):
+            rect = Rectangle(3, 4, 5.3)
+
+    def test_y_float(self):
+        """Test y setter functions with strings"""
+        with self.assertRaises(TypeError):
+            rect = Rectangle(3, 4, 5, 1.3)
+
+    def test_width_setter2(self):
         """Try to set width and height to 0"""
-
         with self.assertRaises(ValueError):
-            self.a = Rectangle(0, 2)
+            a = Rectangle(0, 2)
+
+    def test_height_setter2(self):
         with self.assertRaises(ValueError):
-            self.a = Rectangle(2, 0)
-
-    def test_setters_with_zero_b(self):
-        """Try to set x and y to 0"""
-
-        self.rect.x = 0
-        self.rect.y = 0
-        self.assertEqual(self.rect.x, 0)
-        self.assertEqual(self.rect.y, 0)
+            a = Rectangle(2, 0)
 
     def test_area(self):
         rect = self.rect
