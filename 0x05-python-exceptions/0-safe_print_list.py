e3eb96ecd43c34ba601131f41fb08ce1133d1f68
Sonlowami <uwilowami64@outlook.com>
2022-12-25 08:39:40 +0200

    Make 2 files executables

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+"""Test safe_print_list
+    
+    >>> safe_print_list([], x = 0)
+    >>>
+    >>> safe_print_list([4], x = 0)
+    4
+    >>> safe_print_list([4, 6], x = 1)
+    6
+    >>> safe_print_list([4, 6, 8], x = 2)
+    8
+    >>> safe_print_list([4, 6, 8, "None"], x = 3)
+    None
+    >>>
+    """
 def safe_print_list(my_list=[], x=0):
     try:
         i = 0
@@ -9,3 +23,7 @@ def safe_print_list(my_list=[], x=0):
         pass
     print("")
     return i
+
+if __name__=='__main__':
+    import doctest
+    doctest.testmod()
