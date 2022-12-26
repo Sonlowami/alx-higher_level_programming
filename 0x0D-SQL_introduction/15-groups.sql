5931f174cb2ccd976eb01d21d5a14aeb27863ebe
Uwimana Lowami <uwilowami64@outlook.com>
2022-12-26 01:06:04 -0800

    Fix bugs with task #15 AND #16

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- List number of records with same score
-SELECT score, COUNT(score) FROM second_table GROUP BY score;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score;
--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- List all table records except those without a name
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
