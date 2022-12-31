58607db37b391876c18a45dc6ec4c00d63d8bb6d
Uwimana Lowami <uwilowami64@outlook.com>
2022-12-31 00:28:31 -0800

    Fix a bug

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- Find all cities of California
-SELECT cities.id, cities.name FROM cities INNER JOIN states WHERE cities.state_id = states.id;
+SELECT cities.id, cities.name FROM cities INNER JOIN states ON cities.state_id = states.id WHERE states.name LIKE 'California' ORDER BY cities.id;
