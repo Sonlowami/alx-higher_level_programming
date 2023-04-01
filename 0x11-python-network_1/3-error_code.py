c4d3306b5ff5b79427fb2b43fbcf1c007a352428
Sonlowami <uwilowami64@outlook.com>
2023-04-01 19:41:27 +0200

    Encapsulate with statement within try block

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,11 +8,12 @@ def main():
     from urllib import request
     from urllib.error import HTTPError
     from sys import argv
-    with request.urlopen("{}".format(argv[1])) as resp:
-        try:
-            print(resp.read().decode("utf-8"))
-        except HTTPError:
-            print("Error code: {}".format(resp.status))
+    try:
+        with request.urlopen("{}".format(argv[1])) as resp:
+            resp = resp.read()
+            print(resp.decode("utf-8"))
+    except HTTPError as error:
+        print("Error code: {}".format(error.status))
 
 
 if __name__ == '__main__':
