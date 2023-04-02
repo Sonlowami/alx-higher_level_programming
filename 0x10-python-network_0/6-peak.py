546ebf5e807934feeec1e441013ab9777ce0f718
Sonlowami <uwilowami64@outlook.com>
2023-04-02 14:24:42 +0200

    Use linear search to solve task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Find the peak in an unsorted array of numbers"""
+
+
+def find_peak(list_of_integers):
+    """Find the peak of an unsorted array"""
+
+    if len(list_of_integers) == 0 or list_of_integers is None:
+        return None
+    ints = list_of_integers[:]
+    peak = ints[0]
+    for num in ints:
+        if num > peak:
+            peak = num
+    return num
