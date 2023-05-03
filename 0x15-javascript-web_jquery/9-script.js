72a36faabe8468d2fec8f4280fb73933794d077b
Sonlowami <uwilowami64@outlook.com>
2023-05-04 00:20:32 +0200

    Fix a bug in 9-script.js

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -3,6 +3,6 @@ $(() => {
     url: 'https://fourtonfish.com/hellosalut/?lang=fr',
     dataType: 'jsonp',
     type: 'GET',
-    success: (data) => { console.log(data); }
+    success: (data) => { $('#hello').text(data.hello); }
   });
 });
